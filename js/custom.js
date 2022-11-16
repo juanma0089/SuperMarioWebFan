04f7adea96b580a82984592dcaa94f397072e31d
Juanma Romalde <juanma0089@hotmail.com>
2022-11-16 22:08:22 +0100

    no solucion de url sound

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
 "use strict";
 
-    let arraySonidos = ['./sound/mario/mario-bros-here-we-go-hoo.mp3','./sound/mario/mario-bros-mamma-mia.mp3',
-    './sound/luigi/luigi.wav','./sound/luigi/luigi2.wav','./sound/peach/peach.wav','./sound/peach/peach2.wav',
-    './sound/toad/toad.m4a','./sound/toad/toad2.m4a','./sound/bowser/Bowser.wav','./sound/bowser/browser2.wav','./sound/yoshi/yoshi.wav','./sound/yoshi/yoshi2.wav'];
+    let arraySonidos = ['../sound/mario/mario-bros-here-we-go-hoo.mp3','../sound/mario/mario-bros-mamma-mia.mp3',
+    '../sound/luigi/luigi.wav','../sound/luigi/luigi2.wav','../sound/peach/peach.wav','../sound/peach/peach2.wav',
+    '../sound/toad/toad.m4a','../sound/toad/toad2.m4a','../sound/bowser/Bowser.wav','../sound/bowser/browser2.wav','../sound/yoshi/yoshi.wav','../sound/yoshi/yoshi2.wav'];
     
 function playMusic(pos){
     let music = new Audio(arraySonidos[pos]);
